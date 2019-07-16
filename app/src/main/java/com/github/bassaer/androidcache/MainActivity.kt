69109a05c63245f907f9863707255da47671b6aa
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-07-16 22:21:04 +0900

    hoge

--- app/src/main/java/com/github/bassaer/androidcache/MainActivity.kt
+++ app/src/main/java/com/github/bassaer/androidcache/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.github.bassaer.androidcache
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.StringWriter
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,25 +18,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        createCache()
+
+        val textView = findViewById<TextView>(R.id.main_text)
+        textView.text = getCacheInfo()
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun createCache() {
+        val str = object : StringWriter() {
+            init {
+                for (i in 1..1024) write("a")
+            }
+        }.toString()
+        log(str)
+        val newFilePath = "${cacheDir.absolutePath}/${System.currentTimeMillis() / 1000}.txt"
+        val file = FileOutputStream(File(newFilePath))
+        val outputStream = BufferedOutputStream(file)
+        outputStream.write(str.toByteArray())
+        log("create file : $newFilePath")
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun getCacheInfo() : String {
+
+        log("cacheDir : ${cacheDir.absoluteFile}")
+        val cacheDirectory =  File(cacheDir.absolutePath)
+        val files = cacheDirectory.listFiles()
+
+        val builder = StringBuilder()
+        builder.append("[Info]\n")
+        builder.append("size: ${cacheDirectory.length()}\n")
+        builder.append("[Dirs]\n")
+        files.forEach {
+            if (it.isDirectory) {
+                builder.append("${it.name}\n")
+            }
         }
+        builder.append("[Files]\n")
+        files.forEach {
+            if (it.isFile) {
+                builder.append("${it.name}\n")
+            }
+        }
+        builder.append("[Done]")
+
+        return builder.toString()
+    }
+
+    private fun log(msg: String) {
+        Log.d("ANDROID_CACHE_LOG", msg)
     }
 }
